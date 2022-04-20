2fc517d646f6a4843bac4805f78b4cc5aa37dfc6
elodie <e.fontaine.1991@hotmail.fr>
2022-04-20 18:16:12 +0200

    creation unique du fichier de migration

---
+++ migrations/Version20220420161221.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220420161221 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE achat (id INT AUTO_INCREMENT NOT NULL, client_id INT DEFAULT NULL, produit_id INT DEFAULT NULL, date_achat DATE NOT NULL, INDEX IDX_26A9845619EB6921 (client_id), INDEX IDX_26A98456F347EFB (produit_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE blog (id INT AUTO_INCREMENT NOT NULL, produit_id INT DEFAULT NULL, user_id INT DEFAULT NULL, contenue LONGTEXT NOT NULL, date_creation DATE NOT NULL, titre VARCHAR(255) NOT NULL, INDEX IDX_C0155143F347EFB (produit_id), INDEX IDX_C0155143A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE commentaire_produit (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, produit_id INT DEFAULT NULL, texte LONGTEXT DEFAULT NULL, date_commentaire DATE NOT NULL, INDEX IDX_5A6D7E74A76ED395 (user_id), INDEX IDX_5A6D7E74F347EFB (produit_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE mail (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, pseudo VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, adresse VARCHAR(255) NOT NULL, code_postale VARCHAR(255) NOT NULL, ville VARCHAR(255) NOT NULL, tel VARCHAR(255) NOT NULL, clef_parrainage VARCHAR(255) NOT NULL, parrain VARCHAR(255) NOT NULL, date_inscription DATE NOT NULL, UNIQUE INDEX UNIQ_5126AC48E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, blog_id INT DEFAULT NULL, contenue LONGTEXT NOT NULL, date_message DATE NOT NULL, INDEX IDX_B6BD307FA76ED395 (user_id), INDEX IDX_B6BD307FDAE07E97 (blog_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prix INT NOT NULL, description LONGTEXT NOT NULL, quantite INT DEFAULT NULL, category VARCHAR(255) NOT NULL, date_de_sortie DATE NOT NULL, image VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE achat ADD CONSTRAINT FK_26A9845619EB6921 FOREIGN KEY (client_id) REFERENCES mail (id)');
+        $this->addSql('ALTER TABLE achat ADD CONSTRAINT FK_26A98456F347EFB FOREIGN KEY (produit_id) REFERENCES produit (id)');
+        $this->addSql('ALTER TABLE blog ADD CONSTRAINT FK_C0155143F347EFB FOREIGN KEY (produit_id) REFERENCES produit (id)');
+        $this->addSql('ALTER TABLE blog ADD CONSTRAINT FK_C0155143A76ED395 FOREIGN KEY (user_id) REFERENCES mail (id)');
+        $this->addSql('ALTER TABLE commentaire_produit ADD CONSTRAINT FK_5A6D7E74A76ED395 FOREIGN KEY (user_id) REFERENCES mail (id)');
+        $this->addSql('ALTER TABLE commentaire_produit ADD CONSTRAINT FK_5A6D7E74F347EFB FOREIGN KEY (produit_id) REFERENCES produit (id)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FA76ED395 FOREIGN KEY (user_id) REFERENCES mail (id)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FDAE07E97 FOREIGN KEY (blog_id) REFERENCES blog (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307FDAE07E97');
+        $this->addSql('ALTER TABLE achat DROP FOREIGN KEY FK_26A9845619EB6921');
+        $this->addSql('ALTER TABLE blog DROP FOREIGN KEY FK_C0155143A76ED395');
+        $this->addSql('ALTER TABLE commentaire_produit DROP FOREIGN KEY FK_5A6D7E74A76ED395');
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307FA76ED395');
+        $this->addSql('ALTER TABLE achat DROP FOREIGN KEY FK_26A98456F347EFB');
+        $this->addSql('ALTER TABLE blog DROP FOREIGN KEY FK_C0155143F347EFB');
+        $this->addSql('ALTER TABLE commentaire_produit DROP FOREIGN KEY FK_5A6D7E74F347EFB');
+        $this->addSql('DROP TABLE achat');
+        $this->addSql('DROP TABLE blog');
+        $this->addSql('DROP TABLE commentaire_produit');
+        $this->addSql('DROP TABLE mail');
+        $this->addSql('DROP TABLE message');
+        $this->addSql('DROP TABLE produit');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
