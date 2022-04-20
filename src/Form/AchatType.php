ca6aea739acaeefc48c5de25e760537434738029
elodie <e.fontaine.1991@hotmail.fr>
2022-04-20 21:32:58 +0200

    ajout des cruds

---
+++ src/Form/AchatType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Achat;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AchatType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date_achat')
+            ->add('client')
+            ->add('produit')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Achat::class,
+        ]);
+    }
+}
