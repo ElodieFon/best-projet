77b2ad5383ae3f6d4d40c66158cb46bdadfa8dd5
Alexandre Todorovic <todorovic.alexandre@yahoo.fr>
2022-04-21 15:06:33 +0200

    modif 21/04 15h06

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -6,6 +6,7 @@ use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class ProduitType extends AbstractType
 {
